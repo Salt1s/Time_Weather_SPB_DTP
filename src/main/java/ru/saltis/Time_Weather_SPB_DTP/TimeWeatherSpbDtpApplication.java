680bf75c8bf00af09a6a1059c96061c10640fa33
Aleksei <alekseibaruzdin@gmail.com>
2025-10-08 19:53:44 +0300

    Initialize project to Repo

---
+++ src/main/java/ru/saltis/Time_Weather_SPB_DTP/TimeWeatherSpbDtpApplication.java
@@ -0,0 +1,13 @@
+package ru.saltis.Time_Weather_SPB_DTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeWeatherSpbDtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeWeatherSpbDtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/saltis/Time_Weather_SPB_DTP/TimeWeatherSpbDtpApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.saltis.Time_Weather_SPB_DTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeWeatherSpbDtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
